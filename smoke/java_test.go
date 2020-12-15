c32922715233d68f38b0dc0d5f78639fbca9bb4c
Frankie Gallina-Jones <frankieg@vmware.com>
2020-12-15 22:40:50 +0000

    updates container run calls to match occam change

--- smoke/java_test.go
+++ smoke/java_test.go
@@ -61,7 +61,10 @@ func testJava(t *testing.T, context spec.G, it spec.S) {
 				Execute(name, source)
 			Expect(err).ToNot(HaveOccurred(), logs.String)
 
-			container, err = docker.Container.Run.Execute(image.ID)
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				Execute(image.ID)
 			Expect(err).NotTo(HaveOccurred())
 
 			Eventually(container).Should(BeAvailable())
