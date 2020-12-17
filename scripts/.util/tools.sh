0ffc46ca3af69019d4b6922faa326ef1e63018e0
Arjun Sreedharan <asreedharan@vmware.com>
2020-12-17 19:47:24 +0000

    Strictly use same pack version throughout

--- scripts/.util/tools.sh
+++ scripts/.util/tools.sh
@@ -50,7 +50,11 @@ function util::tools::pack::install() {
 
   if [[ ! -f "${dir}/pack" ]]; then
     local version
-    version="v0.15.0"
+    if [[ ! -f "${BUILDERDIR}/.github/pack-version" ]]; then
+      util::print::error "Pack version file (.github/pack-version) not found"
+    fi
+
+    version="v$(cat "${BUILDERDIR}"/.github/pack-version)"
 
     util::print::title "Installing pack ${version}"
     curl "https://github.com/buildpacks/pack/releases/download/${version}/pack-${version}-${os}.tgz" \
