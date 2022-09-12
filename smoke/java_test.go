749387678d2e750f5f4b8242b3b72dafe4976936
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-09-12 14:55:19 +0000

    Revert some name checks

--- smoke/java_test.go
+++ smoke/java_test.go
@@ -70,8 +70,8 @@ func testJava(t *testing.T, context spec.G, it spec.S) {
 			Eventually(container).Should(BeAvailable())
 
 			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for CA Certificates")))
-			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for BellSoft Liberica")))
-			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for Executable JAR")))
+			Expect(logs).To(ContainLines(ContainSubstring("Paketo BellSoft Liberica Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Paketo Executable JAR Buildpack")))
 			Expect(logs).To(ContainLines(ContainSubstring("Paketo Buildpack for Spring Boot")))
 		})
 	})
